9471d41652344f630b1fef57082fdf256b2eefaa
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2026-04-17 13:54:25 +0200

    dilemma

---
+++ code/cpp/dilemmaGenerator.c
@@ -0,0 +1,31 @@
+// generate hard-to-round cases for sinf in [0.5,1)
+// with at least 20 identical bits after the round bit
+
+#include <stdio.h>
+#include <mpfr.h>
+
+static void
+check (mpfr_t x) {
+ mpfr_t y, z;
+ mpfr_init2 (y, 24 + 20);
+ mpfr_init2 (z, 25);
+ mpfr_sin (y, x, MPFR_RNDN);
+ mpfr_set (z, y, MPFR_RNDN);
+ if (mpfr_cmp (y, z) == 0)
+   mpfr_printf ("%Ra\n", x);
+ mpfr_clear (z);
+ mpfr_clear (y);
+}
+
+int main()
+{
+ mpfr_t x;
+ mpfr_init2 (x, 24);
+ mpfr_set_d (x, 0.5, MPFR_RNDN);
+ while (mpfr_cmp_ui (x, 1) < 0) {
+   check (x);
+   mpfr_nextabove (x);
+ }
+ mpfr_clear (x);
+}
+
