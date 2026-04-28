2cad27d756846f144b7976d25e7a1665f5007fcb
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2026-04-28 15:27:59 +0200

    more fun facts

--- code/cpp/acos.cpp
+++ code/cpp/acos.cpp
@@ -7,10 +7,13 @@ int main(int argc, char** argv) {
   float one = argc;
   float piw = acosf(-one);  
   double pid = piw;  
+  float pi = 0x1.921fb6p+1;
   
   std::cout << std::scientific << std::setprecision(8) << pid << std::endl;
   std::cout << std::pow(9.f*9.f + 19.f*19.f/22.f,0.25f) << std::endl;
   std::cout << std::exp(piw) -piw << std::endl;
+  std::cout << std::exp(pi) -pi << std::endl;
+  std::cout << exp(M_PI) -M_PI << std::endl;
   std::cout << std::hexfloat << piw << std::endl;
   std::cout << std::pow(9.f*9.f + 19.f*19.f/22.f,0.25f) << std::endl;
   std::cout << std::exp(piw) -piw << std::endl;
